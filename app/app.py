908400145c894e58bbbf5165cc03f21aa3492d9b
turvova1989@gmail.com <TurVova>
2017-09-04 22:39:36 +0300

    Two

--- app/app.py
+++ app/app.py
@@ -7,4 +7,8 @@ from app import views
 
 app.add_url_rule('/', 'index', views.index)
 app.add_url_rule('/gen-pass/<int:pass_len>/<int:pass_count>', 'gen_pass', views.gen_pass)
-# app.add_url_rule('/all-customers', views.exec_query)
+app.add_url_rule('/customers/', 'customers', views.customers)
+app.add_url_rule('/id-customer/<int:customer_id>/', 'id_customer', views.id_customer)
+app.add_url_rule('/country-customers/<country>/', 'country', views.country_customers)
+app.add_url_rule('/search-phone', 'phone', views.search_phone)
+app.add_url_rule('/filter', 'filter', views.db_filter)
--- app/views.py
+++ app/views.py
@@ -1,11 +1,10 @@
-from flask import render_template
-# from flask import request
+from flask import render_template, request
 from app.utils import id_generator
-# from app.utils import all_customers
-# from app.utils import customer_id
-# from app.utils import customers_country
-# from app.utils import phones_search
-# from app.utils import filter_db
+from app.utils import all_customers
+from app.utils import customer_id
+from app.utils import customers_country
+from app.utils import phones_search
+from app.utils import filter_db
 
 def index():
     return render_template('index.html')
@@ -18,5 +17,31 @@ def gen_pass(pass_len, pass_count):
     }
     return render_template('passwords.html', context=context)
 
-# def base():
-#     return render_template("all_customers.html")
+def customers():
+    context = all_customers()
+    return render_template("customers.html", context=context)
+
+def id_customer(customerid):
+    context = customer_id(customerid)
+    return render_template("customers.html", context=context)
+
+def country_customers(country):
+    context = customers_country(country)
+    return render_template("customers.html", context=context)
+
+def search_phone(phone):
+    phone = request.args.get('phone')
+    context = phones_search(phone)
+    return render_template("customers.html", context=context)
+
+def db_filter():
+    country = request.args.get('country')
+    state = request.args.get('state')
+    city = request.args.get('city')
+    '''params = {
+        'Country' : country
+        'State' : state
+        'City' : city
+    }'''
+    context = filter_db(country, state, city)
+    return render_template('customers.html', context=context)
